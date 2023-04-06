6d329b28ad8f4e460ae1cf7b99e589ae12296458
Brayden <braydenchristensen6@gmail.com>
2023-04-06 00:41:35 -0600

    Changes to be committed: 	new file:   .gitignore 	modified:   package-lock.json 	modified:   package.json 	modified:   playwright.config.js 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		watch: {
+			usePolling: true,
+		},
+		host: true, //Docker
+		strictPort: true,
+		port: 3000,
+	}
 };
 
 export default config;
